28547f191df6e0443998b00140ed8f3a7a093420
Arcel-S <marcellsiregar1204@student.itera.ac.id>
2025-11-10 11:24:07 +0700

    Add Chapter 05 - Unit Tests and pytest

---
+++ 05 Unit Tests and pytest/unit_testing/tutorial/__init__.py
@@ -0,0 +1,13 @@
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+def hello_world(request):
+    return Response('<body><h1>Hello World!</h1></body>')
+
+def main(global_config, **settings):
+    """This function returns a Pyramid WSGI application."""
+    with Configurator(settings=settings) as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        # Return the WSGI application so pserve (paste) can run it.
+        return config.make_wsgi_app()
